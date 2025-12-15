e56b3721c1c066cde895d5334383c6df41993b91
nhanlt-FLS <nhan.letrong@floatingcube.com>
2025-12-15 18:56:16 +0700

    Hide logout button in my account page

--- src/wp-content/themes/zippy-child/inc/custom-action/custom.php
+++ src/wp-content/themes/zippy-child/inc/custom-action/custom.php
@@ -1,7 +1,13 @@
 <?php
-add_action ('woocommerce_before_checkout_form', function () {
+add_action('woocommerce_before_checkout_form', function () {
     echo '<h2 class="checkout-title">Checkout</h2>';
 });
-add_action ('woocommerce_before_cart', function () {
+add_action('woocommerce_before_cart', function () {
     echo '<h2 class="checkout-title">Cart</h2>';
+});
+
+add_action('template_redirect', function () {
+    if (is_account_page()) {
+        wc_clear_notices();
+    }
 });
