773111c9fd8c98166139f765a2a9ce34cfc1eb15
nhanlt-FLS <nhan.letrong@floatingcube.com>
2025-12-16 16:19:27 +0700

    Optimise UI for some pages

--- src/wp-content/themes/zippy-child/inc/custom-action/custom.php
+++ src/wp-content/themes/zippy-child/inc/custom-action/custom.php
@@ -5,3 +5,9 @@ add_action('woocommerce_before_checkout_form', function () {
 add_action('woocommerce_before_cart', function () {
     echo '<h2 class="checkout-title">Cart</h2>';
 });
+
+
+add_filter('woocommerce_cart_item_permalink', function ($permalink, $cart_item, $cart_item_key) {
+    return false;
+}, 10, 3);
+
